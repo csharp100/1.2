c43a85f68a252cf12dd37426672386798127051f
csharp100 <clint63012@hotmail.com>
2014-03-16 09:34:20 -0500

    Exercise 1.2 Liang Java 8E Comprehensive Version Initial Commit

---
+++ src/pkg1/pkg2/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg1.pkg2;
+
+/**
+ *
+ * @author Clint Sharp
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
